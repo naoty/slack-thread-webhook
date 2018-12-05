f4d7567f41806961f0fcfca681d0df31349effcf
Naoto Kaneko <naoty.k@gmail.com>
2018-12-05 22:47:01 +0900

    Add tests for help and version flag

---
+++ cli_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestHelp(t *testing.T) {
+	cli := cli{outStream: bytes.NewBufferString(""), errStream: bytes.NewBufferString("")}
+	status := cli.Run([]string{"slack-thread-webhook", "--help"})
+	if status != 0 {
+		t.Errorf("expected: 0, got: %d\n", status)
+	}
+}
+
+func TestVersion(t *testing.T) {
+	cli := cli{outStream: bytes.NewBufferString(""), errStream: bytes.NewBufferString("")}
+	status := cli.Run([]string{"slack-thread-webhook", "--version"})
+	if status != 0 {
+		t.Errorf("expected: 0, got: %d\n", status)
+	}
+}
