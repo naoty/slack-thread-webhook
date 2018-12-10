291d266b6216656a6553848d851673f3dd6fc14f
Naoto Kaneko <naoty.k@gmail.com>
2018-12-10 22:42:56 +0900

    Post thread messages when a message is already posted

--- handler.go
+++ handler.go
@@ -16,18 +16,29 @@ type handler struct {
 
 func (handler handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	messageParams := slack.NewPostMessageParameters()
-	_, ts, err := handler.slack.PostMessage("general", "Hello", messageParams)
-	if err != nil {
-		message := fmt.Sprintf("failed to post a message to slack: %v\n", err)
-		http.Error(w, message, http.StatusInternalServerError)
-	}
-
 	requestParams := req.Context().Value(paramsKey).(map[string]string)
 	id := requestParams["id"]
-	err = handler.datastore.Set(id, ts)
-	if err != nil {
-		message := fmt.Sprintf("failed to set id: %v\n", err)
-		http.Error(w, message, http.StatusInternalServerError)
+	value, _ := handler.datastore.Get(id)
+
+	if value == "" {
+		_, ts, err := handler.slack.PostMessage("general", "Hello", messageParams)
+		if err != nil {
+			message := fmt.Sprintf("failed to post a message to slack: %v\n", err)
+			http.Error(w, message, http.StatusInternalServerError)
+		}
+
+		err = handler.datastore.Set(id, ts)
+		if err != nil {
+			message := fmt.Sprintf("failed to set id: %v\n", err)
+			http.Error(w, message, http.StatusInternalServerError)
+		}
+	} else {
+		messageParams.ThreadTimestamp = value
+		_, _, err := handler.slack.PostMessage("general", "Hello", messageParams)
+		if err != nil {
+			message := fmt.Sprintf("failed to post a message to slack: %v\n", err)
+			http.Error(w, message, http.StatusInternalServerError)
+		}
 	}
 
 	w.WriteHeader(http.StatusCreated)
